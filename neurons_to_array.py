06b70600b343156015dfe2e13baf11bdb57b6b7d
Alexandra Kim <alexandrasuriya@gmail.com>
2022-06-30 15:30:04 -0700

    Modified column names and order

--- neurons_to_array.py
+++ neurons_to_array.py
@@ -1,7 +1,13 @@
+'''
+Program that converts a csv file from Napari to a 3D Numpy array.
+'''
+
 import pandas as pd
 
-df = pd.read_csv('example_neurons.csv',usecols= ['axis-0','axis-1','axis-2'])
-
+filename = 'example_neurons.csv'
+df = pd.read_csv(filename,usecols= ['axis-0','axis-1','axis-2'])
+df.columns = ['Z','X','Y']
+df = df[['X','Y','Z']]
 df.to_numpy()
 
 print(df)
