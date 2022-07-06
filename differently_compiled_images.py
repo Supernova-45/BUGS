61f3022e99aa8b93c577c14dd460456dc17e8d4b
Alexandra Kim <alexandrasuriya@gmail.com>
2022-07-05 17:10:38 -0700

    Added statistical functions with tifffile

--- differently_compiled_images.py
+++ differently_compiled_images.py
@@ -1,8 +1,24 @@
-from PIL import Image
+import tifffile as tif
 import numpy as np
 
-im = Image.open('example_movie.tif')
-im.show()
+def readFile(filename):
+    im = tif.imread(filename)
+    return im
 
-arr = np.array(im)
-print(arr.shape)
+def writeFile(arr):
+    tif.imwrite('output_image.tif',arr,photometric='minisblack')
+
+def standardDeviation(arr):
+    return np.std(arr,axis=0)
+    
+def mean(arr):
+    return np.mean(arr,axis=0)
+
+def main():
+    im = readFile('/Users/alexandrakim/Desktop/BUGS2022/example_movie.tif')
+    # realStdIm = readFile('/Users/alexandrakim/Desktop/BUGS2022/Denoised_STD_20220419_ok03_abtl_h2bcamp6s_7dpf_2v3v5_2P (2).tif')
+    stdIm = standardDeviation(im)
+    print(stdIm)
+
+if __name__ == "__main__":
+    main()
