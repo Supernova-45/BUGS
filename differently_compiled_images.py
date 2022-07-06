2bf168e49221b6b87e305ebbb2faf1a705caa3ee
Alexandra Kim <alexandrasuriya@gmail.com>
2022-07-05 22:45:41 -0700

    Fixed writeFile functionality

--- differently_compiled_images.py
+++ differently_compiled_images.py
@@ -5,20 +5,25 @@ def readFile(filename):
     im = tif.imread(filename)
     return im
 
-def writeFile(arr):
-    tif.imwrite('output_image.tif',arr,photometric='minisblack')
+def writeFile(arr, filename):
+    tif.imwrite(filename, arr, photometric='minisblack')
 
 def standardDeviation(arr):
-    return np.std(arr,axis=0)
+    return np.std(arr,axis=0,dtype='float32')
     
 def mean(arr):
-    return np.mean(arr,axis=0)
+    return np.mean(arr,axis=0,dtype='float32')
+
+def median(arr):
+    return np.median(arr,axis=0,dtype='float32')
+
+def variance(arr):
+    return np.var(arr,axis=0,dtype='float32')
 
 def main():
     im = readFile('/Users/alexandrakim/Desktop/BUGS2022/example_movie.tif')
-    # realStdIm = readFile('/Users/alexandrakim/Desktop/BUGS2022/Denoised_STD_20220419_ok03_abtl_h2bcamp6s_7dpf_2v3v5_2P (2).tif')
     stdIm = standardDeviation(im)
-    print(stdIm)
+    writeFile(stdIm, 'output_image.tif')
 
 if __name__ == "__main__":
     main()
