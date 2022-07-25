d286225222b3599281475149d3f1ec94a30fbf41
Alexandra Kim <alexandrasuriya@gmail.com>
2022-07-25 12:35:08 -0700

    Added example slice

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # from image_manipulation import imageManipulation, arrayManipulation
 from cropper import Cropper, Slices
 
+import tifffile as tif
+
 def main():
-    im = imageManipulation('/Users/alexandrakim/Desktop/BUGS2022/example_movie.tif')
-    print(im.getArr())
+    im = tif.imread("data/example_STD_image.tif")
+    slice = Slices([0,4,4], [17,91,66], im)
+    print(slice.crop(im,"data/example_STD_image.tif"))
+    tif.imwrite("data/example_slice.tif", slice, photometric='minisblack')
 
 if __name__ == "__main__":
     main()
